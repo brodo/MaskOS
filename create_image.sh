d5c2cd9d2b83be05f197073d66bae5995680cfea
Julian Dax <julian.dax@posteo.de>
2022-10-29 01:06:33 +0200

    fix iso build script

--- create_image.sh
+++ create_image.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 rm -rf target/assets || true
 mkdir target/assets
-cp assets/* target/assets
+cp assets/* target/assets/
+cp target/x86_64-unknown-uefi/release/uefi_app.efi target/assets/
 
 hdiutil create -srcfolder target/assets -volname MASKOS -fs FAT32 -ov -format UDTO -layout NONE maskos
